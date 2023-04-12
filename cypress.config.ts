3de5b6022b115173072b96251956e59ac1d6c21c
Diana Carolina Gutierrez Contreras <diana.gutierrezc@perficient.com>
2023-04-12 15:23:37 -0500

    Setting up tests with TypeScript

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      // e2e testing node events setup code
+      return config;
+    },
+  },
+});
