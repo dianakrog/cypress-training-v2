cb70b38f407395c27adcd9522ae72e97f765e97d
Diana Carolina Gutierrez Contreras <diana.gutierrezc@perficient.com>
2023-04-18 09:45:31 -0500

    Configurar Integración Continua (CI)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
+      config.defaultCommandTimeout = 10000;
+      config.responseTimeout = 20000;
+      config.pageLoadTimeout = 60000;
       return config;
-    },
+    }
   },
 });
