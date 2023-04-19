b15892a1c23e8a3f3d0ae406d80c6dd2c08b59b5
Diana Carolina Gutierrez Contreras <diana.gutierrezc@perficient.com>
2023-04-18 19:14:49 -0500

    CSS selectors

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -1,25 +1,21 @@
 describe('Buy a black t-shirt', () => {
     it('then the t-shirt should be bought', () => {
-        cy.visit('https://www.saucedemo.com/') // (1)
-        cy.get('.login-box > form > div > input#user-name').type('standard_user') // (2)
-        cy.get('.login-box > form > div > input#password').type('secret_sauce') // (2)
-        cy.get("input[type='submit']").click() // (2)
-
-        cy.get('#item_1_img_link').click() // (3)
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click() // (4)
-        cy.get('#shopping_cart_container > a').click() // (5)
-        cy.get('#checkout').click() // (6)
-
-        cy.get('#first-name').type('Cypress') // (7)
-        cy.get('#last-name').type('Workshop') // (8)
-        cy.get('#postal-code').type('00000') // (9)
-        cy.get('#continue').click() // (10)
-
-        cy.get('#finish').click() // (11)
-
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('.login-box > form > div > input#user-name').type('standard_user')
+        cy.get('.login-box > form > div > input#password').type('secret_sauce')
+        cy.get("input[type='submit']").click()
+        cy.get('#item_1_img_link').click()
+        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
+        cy.get('#shopping_cart_container > a').click()
+        cy.get('#checkout').click()
+        cy.get('#first-name').type('Cypress')
+        cy.get('#last-name').type('Workshop')
+        cy.get('#postal-code').type('00000')
+        cy.get('#continue').click()
+        cy.get('#finish').click()
         cy.get('#contents_wrapper > .checkout_complete_container > h2').should(
             'have.text',
             'Thank you for your order!'
-        ) // (12)
+        )
     })
 })
